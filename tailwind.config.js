bdf0bae5310fa8733783e31a766e37ab9670be55
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-29 20:28:51 +0300

    Add tailwind and its config to app

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '480px',
+      'md': '547px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1680px',
+    },
+    extend: {
+      colors:{
+        'primary':"#64e3e3"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
